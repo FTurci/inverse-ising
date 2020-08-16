b01da8244f76a016dd26f26e78dfb13f6becd3f4
Francesco Turci <turci@me.com>
2020-08-16 14:55:48 +0100

    Implemented analytic gradient

--- example.py
+++ example.py
@@ -3,16 +3,17 @@ from scipy import optimize
 import h5py
 import matplotlib.pyplot as plt
 import tqdm
-
+import cvxpy
 import src.models as mod
-from src.inverseising import pseudo_loglikelihood
+from src.inverseising import pseudo_loglikelihood, pseudo_loglikelihood_grad, numgrad
 
 produce_data = False
+N = 200			
 
 if produce_data:
-	sk = mod.SK(10,1, outputfile ='data.hdf5')
+	sk = mod.SK(N,1, outputfile =f'dataN{N}.hdf5')
 	beta = 1.0
-	mcsweeps =10**6
+	mcsweeps =5*10**5
 
 	for sweep in tqdm.tqdm(range(mcsweeps)):
 		sk.sweep(beta)
@@ -20,13 +21,14 @@ if produce_data:
 			sk.store()
 
 
-	d = mod.load_data("data.hdf5")
-	J = mod.load_J("data.hdf5")
-	np.savez("data.npz",d, J)
+	d = mod.load_data(f"dataN{N}.hdf5")
+	J = mod.load_J(f"dataN{N}.hdf5")
+	np.savez(f"dataN{N}.npz",d, J)
 
 
-data = np.load('data.npz')['arr_0']
-J =  np.load('data.npz')['arr_1']
+data = np.load(f'dataN{N}.npz')['arr_0'][::10]
+print("Data", data.shape)
+J =  np.load(f'dataN{N}.npz')['arr_1']
 
 row, col = np.triu_indices(data.shape[1],k=1)
 Ju = J[row,col]
@@ -35,16 +37,28 @@ print("J", Ju)
 
 beta = 1.0
 
-ps = pseudo_loglikelihood(Ju,spin=data,beta=beta)
-print ('Pseduo logliklelihood', ps)
-
-res = optimize.minimize(pseudo_loglikelihood,x0=np.ones(len(Ju)),args=(data,beta), 
-	method='L-BFGS-B',
+res = optimize.minimize(pseudo_loglikelihood,x0=np.ones(len(Ju)),args=(data,beta),
+	jac = pseudo_loglikelihood_grad,	
+	# method ='CG' , # 3 sec
+	method ='Newton-CG' ,
+	# method = 'Nelder-Mead'
+	# method='L-BFGS-B' # 25 sec
+	# method = 'Powell', # 90 sec
+	# method = 'BFGS'  # 44 sec
+	# method = 'TNC'	# >245 sec, does not converge
+	# method = 'COBYLA'	#40 sec
+	# method = 'SLSQP'	#22 sec
+	# method = 'trust-constr' #112 sec
+	options={ 'disp':True,'maxiter':20}#, 'gtol':1e-5}
 	)
 
-print('Ju',Ju)
-print('Res', res.x, res.fun)
 
+
+
+print('Ju',Ju)
+
+print('Res', res.x)	
+print(res)
 plt.plot(Ju, res.x,'o')
 plt.plot(Ju,Ju, 'k:')
 plt.xlabel("Original J")
--- src/inverseising.py
+++ src/inverseising.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 """ Based on PRL 108, 090201 (2012)"""
+count = 0
 
 def pseudo_loglikelihood(Jtriu, spin=None, beta=None):
 	N = spin.shape[1]
@@ -21,7 +22,42 @@ def pseudo_loglikelihood(Jtriu, spin=None, beta=None):
 		axis=0
 		)
 	
+
 	assert len(f)==N, 'Wrong dimensions'
+	# print (f.sum())
+	# print(Jtriu, Jtriu.min())
 	return f.sum()
 
+def pseudo_loglikelihood_grad(Jtriu, spin=None, beta=None):
+	global count
+	print(count,"evaluation")
+	N = spin.shape[1]
+	B = spin.shape[0]
 
+	# require Jij = Jji
+	J = np.zeros((N,N))
+	row, col = np.triu_indices(N,k=1)
+
+	J[row,col] = Jtriu
+	J += J.T
+
+	# print(J)
+	prod = -2*beta*spin[:,:,None]*spin[:,None,:]*J
+
+	# print(prod)
+	idx = (J!=0).astype(int)
+	p = 1./(1+np.exp(prod))
+
+	dpdj  = 2.0*beta*(spin[:,:,None]*spin[:,None,:]*idx)*p**2*np.exp(prod)
+
+
+	analitic = np.mean(1/p*dpdj, axis=0)
+	# print("analitic shape",analitic.shape)
+	print("...ended")
+	
+	count+=1
+
+	return -2*analitic[row,col]
+
+def numgrad(Ju, delta,spin=None, beta=None):
+	return	(pseudo_loglikelihood(Ju+delta, spin, beta)-pseudo_loglikelihood(Ju, spin, beta))/delta
